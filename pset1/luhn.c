f715a9ed79c31f57e6127e600e94a9d686efae05
Mark Struzinski <ski081@gmail.com>
2017-02-23 22:28:17 -0500

    Finishing luhn algorithm

--- pset1/luhn.c
+++ pset1/luhn.c
@@ -11,18 +11,22 @@
 
 */
 int digit_sum(int number);
+int get_card_type(int first_digit, int second_digit);
+void print_invalid();
 
-enum {
+typedef enum {
 	AMEX = 0,
 	MC = 1,
-	VISA = 2
+	VISA = 2,
+	INVALID = 4
 } card_type;
 
 int main(int argc, char *argv[]) {
 	int count = 0;
+	long long ccnum = 5111222233334444;
 //	long long ccnum = 4430470093045844;
 //	long long ccnum = 5105105105105100;
-	long long ccnum = 378282246310005;
+//	long long ccnum = 378282246310005;
 	long long tmp = ccnum;
 	
 	while (tmp > 0LL) {
@@ -33,7 +37,7 @@ int main(int argc, char *argv[]) {
 	long long scratch_num = ccnum;
 	int odd_numbers_sum = 0;	
 	int product_digit_sum = 0;
-	int first_position = count
+	int first_position = count;
 	int second_position = count - 1;
 	
 	for (int x = 1; x <= count; x++) {
@@ -49,25 +53,81 @@ int main(int argc, char *argv[]) {
 		
 		scratch_num /= 10LL;		
 		
-		if (x == first_position) {
-			
-		} else if (x == second_position) {
-			
+		if (x == count) {
+			first_position = digit;
+		} else if (x == (count - 1)) {
+			second_position = digit;
 		}
 	}
 	
-	printf("odd_numbers_sum: %i\n", odd_numbers_sum);
-	printf("product_digit_sum: %i\n", product_digit_sum);
+//	printf("first_position: %i\n", first_position);
+//	printf("second_position: %i\n", second_position);
 	
+	card_type ct = get_card_type(first_position, second_position);
+
 	int result = odd_numbers_sum + product_digit_sum;
 	int final_mod = result % 10;
-	
-	if (final_mod == 0) {
-		printf("VALID!!!");
-	} else {
-		printf("INVALID, YA BASTAGE!!!");
+
+	switch (ct) {
+		case AMEX:
+			if (final_mod == 0) {
+				printf("AMEX\n");
+			} else {
+				print_invalid();
+			}
+			break;
+		case MC:
+			if (final_mod == 0) {
+				printf("MASTERCARD\n");				
+			} else {
+				print_invalid();
+			}			
+			break;
+		case VISA:
+			if (final_mod == 0) {
+				printf("VISA\n");
+			}	
+			break;
+		case INVALID:
+			printf("INVALID\n");
+			break;
 	}
+	
+//	printf("card_type: %i\n", ct);
+//	
+//	printf("odd_numbers_sum: %i\n", odd_numbers_sum);
+//	printf("product_digit_sum: %i\n", product_digit_sum);
+	return 0;
 }
+
+void print_invalid() {
+	printf("INVALID\n")
+}
+
+int get_card_type(int first_digit, int second_digit) {
+	int card_code = 4;
+	switch (first_digit) {
+		case 3:
+			if (second_digit == 4 || second_digit == 7) {
+				card_code = 0;
+			} 
+			break;
+		case 4:
+			card_code = 2;
+			break;
+		case 5:
+			if (second_digit >= 1 && second_digit <= 5) {
+				card_code = 1;
+			}
+			break;
+		default:
+			return 4;
+			break;			
+	}
+	
+	return card_code;
+}
+
 
 int digit_sum(int number) {
 	if (number == 0) {
